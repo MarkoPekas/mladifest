5b4c756ffcc381d230f2cf01952ebed256b647e9
Marko Pekas <45564032+MarkoPekas@users.noreply.github.com>
2022-07-31 23:16:38 +0200

    fd

--- components/Hero.js
+++ components/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
       </div>
       <div className="flex flex-col items-start mt-12 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
         <span className="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase"> Međugorje </span>
-        <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl">Međunarodni festival mladih <span classNameName="text-blue-500">"Mladifest"</span></h1>
+        <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl">Međunarodni festival mladih <span classNameName="text-blue-500">{'"Mladifest"'}</span></h1>
         <p className="mb-8 text-base leading-relaxed text-left text-gray-500">Međunarodni susret mladih u Međugorju je svake godine od 31.7. do 6.8. Više od 50.000 mladih i 400 svečenika s cijelog svijeta.</p>
         <div className="flex flex-wrap w-full mt-2 -mx-4 text-left">
           {/* <div className="w-1/4 p-4 mt-4 sm:w-1/4">
